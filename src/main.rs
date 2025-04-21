7b0f3643993b570702bea9cdd252e09548fcd74b
YannickMyxe <shawn.froste.sf@gmail.com>
2025-04-21 11:19:02 +0200

    Added file-handling by command-line arguments

--- src/main.rs
+++ src/main.rs
@@ -11,16 +11,17 @@ mod tree;
 mod parser;
 
 fn main() -> Result<(), LunalaErrors> {
-    println!("Starting Lunala!");
-    let mut contents = String::new();
-    /*
-    let path = Path::new("./Lunala/examples/hello.luna");
-    let mut file = File::open(&path).expect("Couldn't open file");
-
-    file.read_to_string(&mut contents).expect("couldn't read file");
+    println!("[Lunala]");
     
-     */
-    contents.push_str("2 + ( 5 - 3 ) - 1");
+    // Collect Command-Line arguments and skip the first one (programPath)
+    let args: Vec<String> = std::env::args().skip(1).collect();
+
+    let mut contents = String::new();
+    if !args.is_empty() {
+        handle_file(&mut contents);
+    } else {
+        handle_repl(&mut contents);
+    }
 
     let mut scanner = Scanner::new(&contents);
     let mut parser = parser::Parser::new(scanner.scan_tokens()?);
@@ -28,7 +29,18 @@ fn main() -> Result<(), LunalaErrors> {
     let expr = parser.parse()?;
     
     println!("Exp=> {}", expr);
-
     
     Ok(())
+}
+
+fn handle_file(buffer: &mut String) {
+    let path = Path::new("./Lunala/examples/hello.luna");
+    let mut file = File::open(path).expect("Couldn't open file");
+
+    file.read_to_string(buffer).expect("couldn't read file");
+}
+
+fn handle_repl(buffer: &mut String) {
+    println!("Lunala> ");
+    buffer.push_str("2 + ( 5 - 3 ) - 1");
 }
